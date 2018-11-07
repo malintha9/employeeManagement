7c5185a4f0c4f4220cf49e19c7c98a561e2e2b68
Malintha Madusanka <malinthamadusanka@Malinthas-MacBook-Pro.local>
2018-11-07 08:56:52 +0530

    ..

--- src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package com.example.employeemanagement.controller;
 
+import com.example.employeemanagement.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+
+
+@RestController
 public class EmployeeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+
+
+
 }
